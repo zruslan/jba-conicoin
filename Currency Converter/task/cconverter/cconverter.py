8cc18b0e497c8bbf9fdf60c983ae03345e35cf2c
zrus <->
2021-02-23 13:56:53 +0700

    stage 3

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,5 +1,5 @@
 # write your code here!
-coins = int(input())
-print(f"I have {coins} conicoins.")
-print(f"{coins} conicoins cost {coins * 100} dollars.")
-print(f"I am rich! Yippee!")
+coins = int(input("Please, enter the number of conicoins you have: "))
+rate = float(input("Please, enter the exchange rate: "))
+dollars = int(coins * rate) if rate.is_integer() else round(coins * rate, 2)
+print("The total amount of dollars: ", dollars)
